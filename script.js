044e5a5ad4205a7513b6cea486bb3aaff72e14bb
Megan McLean <425megs@gmail.com>
2022-01-09 13:58:27 -0800

    minor tweaks, updated comments

--- script.js
+++ script.js
@@ -4,35 +4,39 @@ $(document).ready(function () {
     $('#currentDay').text(today);
 })
 
-// This should pull the name out of data-time
-var label = document.querySelector('label')
-var time = document.querySelector("#hour")
-label.dataset.time
-
+// Do I need 2 variables to pull the number out of data-time? If I don't include the label element I get "undefined" error
+var element = document.querySelector("label");
 // I think this will pull the current time for me to compare to 
 var timeNow = moment().format("LT");
 
-// need to do moment to pull the current time, then use the for loop to compare the moment data to the data-time
-// only need the hour highlighted when the page refreshes, not running all day
-// Function that will compare something against the hour to know which section to highlight (past, present, future)
-
+// TODO: Function that will use moment to pull the current time, then use the for loop to compare the moment data to the data-time and highlight past/present/future accordingly) -- doesn't seem to be working
 // function highlight() {
-//     for (let i = 0; i < 9; i++)
+//     var time = element.getAttribute("data-time");
+//     var timeNow = moment().format("LT");
+//     for (let i = 0; i < 9; i++) 
 //         if (time < timeNow) {
-//             console.log("hi")
+//             time.setAttribute(".past");
 //         } else if (time === timeNow) {
-//                 then style accorng to .present
+//             time.setAttribute(".present");
 //         }   else { 
-//                 then style with .future
+//             time.setAttribute(".future");        
 //         }
 // }
 
 // Want the save button to push to localStorage
-// function saveButton(event) {
-//     var toDo = $('textarea')
-//     event.preventDefault();
-// }
+function saveButton(event) {
+    var toDo = $('textarea')
+    event.preventDefault();
 
-highlight();
-// JSON parse when pulling
-// JSON stringify when saving to local storage
+    localStorage.setItem("textSection", toDo);
+}
+//this is CLOSE ^ 
+
+function displayToDos() {
+    var toDo = localStorage.getItem("toDo")
+}
+
+
+// Call one highlight function works
+// highlight();
+
